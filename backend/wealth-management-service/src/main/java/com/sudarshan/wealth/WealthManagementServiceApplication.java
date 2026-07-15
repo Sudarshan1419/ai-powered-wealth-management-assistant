ffd7d3a52608ef617e77f3a5598bccb6a13f337d
Sudarshan1419 <sudarshanbhise12345@gmail.com>
2026-07-16 01:52:42 +0530

    Added HelloController and deployed Spring Boot application

---
+++ backend/wealth-management-service/src/main/java/com/sudarshan/wealth/WealthManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sudarshan.wealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthManagementServiceApplication.class, args);
+	}
+
+}
